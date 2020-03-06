34db9b057842b9a138f4926bf5e59219a652e810
GiandoDev <giandomenicoriceputi@gmail.com>
2020-03-06 11:29:53 +0100

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Login({ setUser }) {
+  const [username, setUsername] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setUser(username);
+  }
+
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={event => setUsername(event.target.value)}
+          type="text"
+          placeholder="Input username"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
