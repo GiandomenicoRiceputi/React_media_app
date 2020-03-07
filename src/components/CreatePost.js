30f30bca786a88e7ee9d64f9bcff56864cedc4f2
GiandoDev <giandomenicoriceputi@gmail.com>
2020-03-07 11:15:43 +0100

    advanced hook

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,38 +1,40 @@
 import React from "react";
+import { PostContext } from "../App";
 
-function CreatePost({ user, handleAddPost,}) {
+function CreatePost({ user }) {
+  const { dispatch } = React.useContext(PostContext);
   const [content, setContent] = React.useState("");
   const [image, setImage] = React.useState(null);
   const imageInputRef = React.useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    const post = { content, image, user };
-    handleAddPost(post);
-    //const newPosts = [post, ...posts];
-    //setPosts(newPosts);
+    const post = { content, image, user, id: Date.now() };
+    // handleAddPost(post);
+    dispatch({ type: "ADD_POST", payload: { post } });
     setContent("");
+    setImage(null);
     imageInputRef.current.value = "";
   }
 
   return (
-    <div>
-      <h1>Create New Post</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add Post Content"
-          onChange={event => setContent(event.target.value)}
-          value={content}
-        />
-        <input
-          type="file"
-          onChange={event => setImage(event.target.files[0])}
-          ref={imageInputRef}
-        />
-        <button type="submit">Submit Post</button>
-      </form>
-    </div>
+      <div>
+        <h1>Create New Post</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+              type="text"
+              placeholder="Add Post Content"
+              onChange={event => setContent(event.target.value)}
+              value={content}
+          />
+          <input
+              type="file"
+              onChange={event => setImage(event.target.files[0])}
+              ref={imageInputRef}
+          />
+          <button type="submit">Submit Post</button>
+        </form>
+      </div>
   );
 }
 
